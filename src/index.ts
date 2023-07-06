e88ea8b320c0515620be0a5190222f77e4c4b0d9
cole rener <63392756+dishbin@users.noreply.github.com>
2023-07-05 22:31:06 -0400

    helmet

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import { connectToDatabase } from './db';
 import { mountRoutes } from './routes';
 
@@ -6,6 +7,7 @@ const app = express();
 const port = process.env.SERVER_PORT;
 
 app.use(express.json());
+app.use(helmet());
 
 connectToDatabase();
 mountRoutes(app);
